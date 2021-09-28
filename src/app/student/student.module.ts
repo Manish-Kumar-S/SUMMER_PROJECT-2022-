3f723f48bae41930e92e74a56a01cfa085ceb643
sivaganesh193 <sivaganeshb193@gmail.com>
2021-09-28 17:30:32 +0530

    start angular

---
+++ src/app/student/student.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+
+@NgModule({
+  declarations: [
+    LoginRegisterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudentModule { }
