ac127cab99f7ff3c7bbcc2febc50ca0a4a84798c
jitiendran <jitiendranjiji2000@gmail.com>
2021-11-16 20:07:40 +0530

    Upcoming Companies Integration - 1

--- src/app/student/upcoming-companies/upcoming-companies.component.ts
+++ src/app/student/upcoming-companies/upcoming-companies.component.ts
@@ -1,15 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { API } from 'src/environments/environment';
 
 @Component({
   selector: 'app-upcoming-companies',
   templateUrl: './upcoming-companies.component.html',
-  styleUrls: ['./upcoming-companies.component.scss']
+  styleUrls: ['./upcoming-companies.component.scss'],
 })
 export class UpcomingCompaniesComponent implements OnInit {
+  companies: any[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get<any>(`${API}/student/drives/upcoming`)
+      .subscribe((data) => console.log(data));
   }
-
 }
