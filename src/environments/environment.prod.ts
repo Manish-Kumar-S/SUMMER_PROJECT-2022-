6ed1e4e921b5a8b3982211e5b36e0b65d3a7706c
Vinayaka Murthy Vijay <vijayvinayak23@gmail.com>
2022-02-07 17:47:17 +0530

    heroku changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true
 };
+
+
+export const API = 'https://cuic-dashboard.herokuapp.com/api/v1'; //Heroku Server
+// export const API = 'http://localhost:9000/api/v1'; //Local Server
+export const IMG_URL = 'assets/img';
+export const SVG_URL = 'assets/svg';
